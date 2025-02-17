3a1b20c8dfd3f2242adc137fda77669f13725f7c
Jerrin John <jjohn7@luc.edu>
2025-02-17 16:57:01 -0600

    add script for downloading HMCV donor transcriptomes

---
+++ HCMV_donor_download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://www.ncbi.nlm.nih.gov/sra/SRX2896360 #Donor 1 (2dpi) transcriptome 
+wget https://www.ncbi.nlm.nih.gov/sra/SRX2896363 #Donor 1 (6dpi) transcriptome
+wget https://www.ncbi.nlm.nih.gov/sra/SRX2896374 #Donor 3 (2dpi) transcriptome
+wget https://www.ncbi.nlm.nih.gov/sra/SRX2896375 #Donor 3 (6dpi) transcriptome
