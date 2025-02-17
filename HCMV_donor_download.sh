f44f207f2d654df63096236b2333990d66c39328
Jerrin John <jjohn7@luc.edu>
2025-02-17 17:00:40 -0600

    added comment about function of script

--- HCMV_donor_download.sh
+++ HCMV_donor_download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#Script to download the necessary transcriptomes
 wget https://www.ncbi.nlm.nih.gov/sra/SRX2896360 #Donor 1 (2dpi) transcriptome 
 wget https://www.ncbi.nlm.nih.gov/sra/SRX2896363 #Donor 1 (6dpi) transcriptome
 wget https://www.ncbi.nlm.nih.gov/sra/SRX2896374 #Donor 3 (2dpi) transcriptome
