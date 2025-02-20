cce7816da463d49177053648e4c6f676b1473c62
Jerrin John <jjohn7@luc.edu>
2025-02-20 11:55:26 -0600

    updated links and created paired fastq

--- HCMV_donor_download.sh
+++ HCMV_donor_download.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 #Script to download the necessary transcriptomes
-wget https://www.ncbi.nlm.nih.gov/sra/SRX2896360 #Donor 1 (2dpi) transcriptome 
-wget https://www.ncbi.nlm.nih.gov/sra/SRX2896363 #Donor 1 (6dpi) transcriptome
-wget https://www.ncbi.nlm.nih.gov/sra/SRX2896374 #Donor 3 (2dpi) transcriptome
-wget https://www.ncbi.nlm.nih.gov/sra/SRX2896375 #Donor 3 (6dpi) transcriptome
+wget https://sra-pub-run-odp.s3.amazonaws.com/sra/SRR5660030/SRR5660030 #Donor 1 (2dpi) transcriptome
+wget https://sra-pub-run-odp.s3.amazonaws.com/sra/SRR5660033/SRR5660033 #Donor 1 (6dpi) transcriptome
+wget https://sra-pub-run-odp.s3.amazonaws.com/sra/SRR5660044/SRR5660044 #Donor 3 (2dpi) transcriptome
+wget https://sra-pub-run-odp.s3.amazonaws.com/sra/SRR5660045/SRR5660045 #Donor 3 (6dpi) transcriptome
+
+#Convert the SRR files to paired end fastq files
+fastq-dump --split-files --gzip SRR5660030
+fastq-dump --split-files --gzip SRR5660033
+fastq-dump --split-files --gzip SRR5660044
+fastq-dump --split-files --gzip SRR5660045
+
+#Remove the SRR files
+find . ! -name "*.fastq.gz" -type f -delete
